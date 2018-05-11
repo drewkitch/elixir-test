4a607522a34e406b2a4014710e50c63e2085a482
Drew Kitch <drew.kitch@gmail.com>
2018-05-11 13:27:13 -0400

    first commit

---
+++ app/config/test.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello, HelloWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :hello, Hello.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  database: "hello_test",
+  hostname: System.get_env("PGHOST"),
+  pool: Ecto.Adapters.SQL.Sandbox
